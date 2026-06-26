fd30d453a8fbbadac1ffafbee027be78ec12cae1
parkhyeonggyu15 <gudrb15@naver.com>
2026-06-26 17:44:55 +0900

    02DAYS-REACT.JS

---
+++ 06/src/05ConditionRendering/Component03.jsx
@@ -0,0 +1,27 @@
+
+
+const Component03 = ({items})=>{
+    console.log('items',items)
+    // console.log('length',items.length)
+
+    //01
+    // return (
+    //     <ul>
+    //         {items && items.map((item,idx)=>{
+    //             console.log('item',item);
+    //             return (
+    //                 <li key={idx}>{item}</li>
+    //             );
+    //         })}
+    //     </ul>
+    // )
+
+    //02
+    return (
+        <ul>
+            {items && items.map((item,idx)=><li key={idx}>{item}</li>)}
+        </ul>
+    )
+}
+
+export default Component03;
---
+++ 06/src/App.js
@@ -0,0 +1,27 @@
+import About from "./06Route/About";
+import Contact from "./06Route/Contact";
+import Home from "./06Route/Home";
+
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1>ROUTE</h1> */}
+
+
+
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Contact/:name?/:age?" element={<Contact />} />
+        </Routes>
+      </Router>
+
+    </div>
+   
+  );
+}
+
+export default App;
