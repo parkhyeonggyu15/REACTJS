cc3f3d433f143eca01f8b9cd4d207baa7f362f1c
parkhyeonggyu15 <gudrb15@naver.com>
2026-06-26 18:26:09 +0900

    02DAYS-REACT.JS

---
+++ 07/src/App.js
@@ -0,0 +1,18 @@
+
+import Parent_01 from "./07Component간통신/Props/Parent";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>컴포넌트 간 통신</h1>
+      <Parent_01/>
+
+
+
+    </div>
+  );
+}
+
+export default App;
