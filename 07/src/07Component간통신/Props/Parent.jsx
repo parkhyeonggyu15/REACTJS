f933ad8fa19198bbe8efd2bf7a211861eee6e33f
parkhyeonggyu15 <gudrb15@naver.com>
2026-06-29 10:31:36 +0900

    03DAYS-REACT.JS

--- 07/src/07Component간통신/Props/Parent.jsx
+++ 07/src/07Component간통신/Props/Parent.jsx
@@ -1,22 +1,31 @@
-import {useState,usetEffect} from "react"
+import { useState, useEffect } from "react"
 import Son from "./Son"
+
+import { useContext } from "react"
+import CustomContext from "../ContextAPI/Context"
 
 // 부모 -> 자식 state 전파 : props 
 // 자식 -> 부모 state 변경 : 부모가 만든 Callback함수를 자식이 사용
 const Parent = () => {
-    const [name,setName] = useState("홍길동")
+    const [name, setName] = useState("홍길동")
 
-    const handleChangeName = ()=>{
-        setName(name)
+    //Context Global Value
+    const {globalState,setGlobalState} = useContext(CustomContext)
+
+    const handleChangeName = (name) => {
+        setName(name);
     }
     return (
         <div>
             <h2>PARENT_01 COMPONENT</h2>
-            부모 상태값 name : {name} <br/>
-            <hr/>
-            <Son 
+            부모 상태값 name : {name} <br />
+            <hr />
+            ContextAPI PARENT에서 전역변수 확인 : {globalState}
+
+            <hr />
+            <Son
                 user={name}
-                handleChangeParentName={handleChangeName} 
+                handleChangeParentName={handleChangeName}
             />
         </div>
     )
