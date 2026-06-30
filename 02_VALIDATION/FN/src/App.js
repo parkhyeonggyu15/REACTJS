56bdb979119f8e100c2b08c94ac33c3f2924c2bd
parkhyeonggyu15 <gudrb15@naver.com>
2026-06-30 12:41:24 +0900

    04DAYS-REACT.JS

---
+++ 02_VALIDATION/FN/src/App.js
@@ -0,0 +1,12 @@
+import Param from "./components/Param";
+import Memo from "./components/Memo";
+
+function App() {
+  return (
+    <div className="App">
+     <Memo />
+    </div>
+  );
+}
+
+export default App;
